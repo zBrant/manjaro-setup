1cf241462f62e7bb1ad142857071eb4027354480
carlos.brant <carlos23brant@hotmail.com>
2025-05-04 00:55:38 -0400

    removing atuin

--- install/terminal/atuin.sh
+++ install/terminal/atuin.sh
@@ -1,7 +1,7 @@
-yay -Sy --noconfirm atuin
+#yay -Sy --noconfirm atuin
 
-if $(gum confirm "Press (Y) to register a new account into Atuin or (N) if want to log back in."); then
-  atuin register
-else
-  atuin login
-fi
+#if $(gum confirm "Press (Y) to register a new account into Atuin or (N) if want to log back in."); then
+  #atuin register
+#else
+  #atuin login
+#fi
