5f0d494a35d490277781c5a39e7c86fe119e2754
carlos.brant <carlos23brant@hotmail.com>
2025-05-04 01:10:03 -0400

    changing git link

--- boot.sh
+++ boot.sh
@@ -28,7 +28,7 @@ yay -Syyuu --noconfirm
 
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
-git clone "${OMAKUB_REPO:-https://github.com/akitaonrails/omakub.git}" ~/.local/share/omakub >/dev/null
+git clone "${OMAKUB_REPO:-https://github.com/zbrant/manjaro-setup.git}" ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
   cd ~/.local/share/omakub
   git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
